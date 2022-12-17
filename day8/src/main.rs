9aa5a4ff548427d6b1804fe5cd0c3aaf3e470cf7
Melker Ulander <melker.ulander@pm.me>
2022-12-17 21:23:27 +0100

    Add day 8, part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -18,6 +18,10 @@ fn split_treeline(treeline: &Vec<Tree>, position: usize) -> (Vec<Tree>, Vec<Tree
     return (left.to_vec(), right.to_vec());
 }
 
+fn get_vertical_treeline(forest: &Forest, x: usize) -> Vec<u32> {
+    return forest.iter().map(|treeline| treeline[x]).collect();
+}
+
 fn is_horizontally_visible(tree: &Coordinate, forest: &Forest) -> bool {
     let (left, right) = split_treeline(&forest[tree.y], tree.x);
     let height = forest[tree.y][tree.x];
@@ -27,10 +31,7 @@ fn is_horizontally_visible(tree: &Coordinate, forest: &Forest) -> bool {
 }
 
 fn is_vertically_visible(tree: &Coordinate, forest: &Forest) -> bool {
-    let treeline: Vec<u32> = forest
-        .iter()
-        .map(|treeline| treeline[tree.x])
-        .collect();
+    let treeline = get_vertical_treeline(&forest, tree.x);
     let (top, bottom) = split_treeline(&treeline, tree.y);
     let height = forest[tree.y][tree.x];
 
@@ -57,20 +58,66 @@ fn part_one() {
         )
         .collect();
 
-    let visible_trees: Vec<bool> = forest.to_owned().iter().enumerate().flat_map(|(y, treeline)| {
-        treeline.iter().enumerate().map(|(x, _)| {
+    let visible_trees: Vec<bool> = forest.iter().enumerate().flat_map(|(y, treeline)|
+        treeline.iter().enumerate().map(|(x, _)|
             is_visible(&Coordinate {x, y}, forest.as_ref())
-        }).collect::<Vec<bool>>()
-    }).collect();
+        ).collect::<Vec<bool>>()
+    ).collect();
 
     let sum = visible_trees
         .into_iter()
         .filter(|tree| *tree)
         .into_iter().count();
 
-    println!("sum: {:?}", sum);
+    println!("Part 1: {:?}", sum);
+}
+
+fn count_visible_trees_to_right(treeline: &Vec<Tree>, height: u32) -> usize {
+    // This had to be imperative since I couldn't find a nice way to do this with iterators
+    let mut visibles = treeline.iter().peekable();
+    let mut count = 0;
+    while let Some(tree) = visibles.next() {
+        count += 1;
+        if *tree >= height { break; }
+    };
+    return count;
+}
+
+fn scenic_score(forest: &Forest, tree: &Coordinate) -> usize {
+    let height = forest[tree.y][tree.x];
+    let (mut left, right) = split_treeline(&forest[tree.y], tree.x);
+    let vertical_treeline = get_vertical_treeline(forest, tree.x);
+    let (mut top, bottom) = split_treeline(&vertical_treeline, tree.y);
+    left.reverse();
+    top.reverse();
+
+    [left, right, top, bottom].iter().map(|treeline| {
+        count_visible_trees_to_right(treeline, height)
+    }).product()
+}
+
+fn part_two() {
+    let file_path = "data.txt";
+    let file = File::open(file_path).expect("File not found");
+    let forest = BufReader::new(file).lines();
+
+    let forest: Forest = forest.map(|treeline| treeline
+        .unwrap()
+        .chars()
+        .map(|tree| tree.to_digit(10).unwrap())
+        .collect()
+    ).collect();
+
+    let highest_scenic_score = forest.iter().enumerate().flat_map(|(y, treeline)|
+        treeline.iter().enumerate().map(|(x, _)|
+            scenic_score(&forest, &Coordinate {x, y})
+        ).collect::<Vec<usize>>()
+    ).max().unwrap();
+
+    println!("Part 2: {:?}", highest_scenic_score);
 }
 
 fn main() {
     part_one();
+    part_two();
 }
