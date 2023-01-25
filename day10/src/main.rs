1faa8eedc0da86ea3d143d0bc573ec6cfe0c48ff
Melker Ulander <melker.ulander@pm.me>
2023-01-25 17:41:58 +0100

    Complete day 10, part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,31 +1,33 @@
 use self::Instruction::{Addx, Noop};
-
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Debug)]
 enum Instruction {
-    Addx(i32),
+    Addx(i32, u32),
     Noop,
 }
 
-/// (cycles, value)
-type Output = (u32, i32);
+enum Output {
+    Instruction(Instruction),
+    Value(i32),
+}
 
 impl Instruction {
     fn from_string(string: &str) -> Self {
         let mut instruction = string.split_whitespace();
         match instruction.next() {
             Some("noop") => Noop,
-            Some("addx") => Addx(instruction.next().unwrap().parse().unwrap()),
+            Some("addx") => Addx(instruction.next().unwrap().parse().unwrap(), 2),
             _ => panic!("No or unexpected word found"),
         }
     }
 
-    fn process(&self) -> Output {
+    fn process(&self, value_total: i32) -> Output {
         match self {
-            Addx(value) => (2, *value),
-            Noop => (1, 0),
+            Addx(value, 1) => Output::Value(value_total + value),
+            Addx(value, cycles) => Output::Instruction(Addx(*value, cycles - 1)),
+            Noop => Output::Value(value_total),
         }
     }
 }
@@ -39,43 +41,34 @@ fn part_one() {
     let file = File::open(file_path).expect("File not found");
     let mut instructions = BufReader::new(file).lines();
 
-    let mut cycles_total = 0;
-    let mut value_total = 0;
+    let mut cycles_total = 1;
+    let mut value_total = 1;
     let mut signal_strengths: Vec<i32> = vec![];
+    let mut current_instruction: Option<Instruction> = None;
 
     while cycles_total <= 220 {
-        let instruction = Instruction::from_string(&instructions.next().unwrap().unwrap());
-        // TODO: we need to be able to probe in the middle of process()
-        let (cycles, value) = instruction.process();
-
-        cycles_total += cycles;
-        value_total += value;
+        let instruction = if let Some(instruction) = current_instruction.take() {
+            instruction
+        } else {
+            Instruction::from_string(&instructions.next().unwrap().unwrap())
+        };
 
         if probe(cycles_total) {
             signal_strengths.push(value_total * cycles_total as i32);
         }
+
+        let output = instruction.process(value_total);
+        match output {
+            Output::Value(value) => value_total = value,
+            Output::Instruction(instruction) => current_instruction = Some(instruction),
+        }
+
+        cycles_total += 1;
     }
+
+    let sum: i32 = signal_strengths.iter().sum();
+    println!("Part 1: {:?}", sum);
 }
-
-// fn part_one() {
-//     let file_path = "data.txt";
-//     let file = File::open(file_path).expect("File not found");
-//     let instructions = BufReader::new(file).lines();
-
-//     let probe_cycles = vec![20, 60, 100, 140, 180, 220];
-//     let mut signal_strengths: Vec<i32> = vec![];
-
-//     let sum = instructions.fold((0, 0), |accum, instruction| {
-//         let instruction = Instruction::from_string(&instruction.unwrap());
-//         let (cycles, value) = instruction.process(accum.1);
-//         if probe_cycles.contains(&accum.0) {
-//             signal_strengths.push(cycles as i32 * value);
-//         }
-//         (cycles + accum.0, value)
-//     });
-//     println!("sum: {:?}", sum);
-//     println!("signal_strengths: {:?}", signal_strengths);
-// }
 
 fn main() {
     part_one();
