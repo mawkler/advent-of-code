de4731f2395a8c06523b24d71e1bc72ae3b0c844
Melker Ulander <melker.ulander@pm.me>
2023-12-28 10:48:21 +0100

    Day 9, part 1: completed

---
+++ 2023/src/bin/day9.rs
@@ -0,0 +1,50 @@
+use itertools::Itertools;
+use std::iter;
+
+fn predict_next_value(triangle: Vec<Vec<i32>>) -> i32 {
+    triangle.iter().rev().fold(0, |diff, numbers| {
+        *numbers
+            .iter()
+            .chain(Some(&(numbers.last().unwrap() + diff)))
+            .last()
+            .unwrap()
+    })
+}
+
+fn build_triangle(numbers: Vec<i32>) -> Vec<Vec<i32>> {
+    iter::successors(Some(numbers), |num| {
+        if num.iter().all(|&num| num == 0) {
+            None
+        } else {
+            Some(get_differences(num))
+        }
+    })
+    .collect()
+}
+
+fn get_differences(numbers: &[i32]) -> Vec<i32> {
+    numbers
+        .iter()
+        .tuple_windows()
+        .map(|(prev, next)| next - prev)
+        .collect()
+}
+
+fn parse_lines(lines: &str) -> Vec<Vec<i32>> {
+    lines
+        .lines()
+        .map(|line| line.split(' ').map(|line| line.parse().unwrap()).collect())
+        .collect()
+}
+
+fn main() {
+    let data = include_str!("../../data/day9");
+    let lines = parse_lines(data);
+    let sum: i32 = lines
+        .into_iter()
+        .map(build_triangle)
+        .map(predict_next_value)
+        .sum();
+
+    println!("Part 1: {}", sum);
+}
