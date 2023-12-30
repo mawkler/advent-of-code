6e8909f999ba79960a1364318b04e72fb5b0811c
Melker Ulander <melker.ulander@pm.me>
2023-12-30 22:48:07 +0100

    Day 10, part 1: add pipe following

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -1,7 +1,55 @@
+use self::Direction::{East, North, South, West};
 use indoc::indoc;
-use std::fmt::Display;
+use std::{fmt::Display, ops::Add};
+
+#[derive(Debug, PartialEq)]
+struct Coordinate(i32, i32);
+
+impl Add for &Coordinate {
+    type Output = Coordinate;
+
+    fn add(self, rhs: &Coordinate) -> Self::Output {
+        Coordinate(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
 
 struct Maze(Vec<Vec<Tile>>);
+
+impl Maze {
+    fn get_tile(&self, Coordinate(x, y): &Coordinate) -> Option<&Tile> {
+        self.0.get(*y as usize).and_then(|row| row.get(*x as usize))
+    }
+
+    fn get_connected_pipe(&self, coordinate: &Coordinate, direction: Direction) -> Option<&Pipe> {
+        let neighbour_coordinate = coordinate + &direction.get_delta();
+
+        self.get_tile(&neighbour_coordinate)
+            .and_then(|neighbour_tile| match neighbour_tile {
+                Tile::Pipe(pipe) => {
+                    let Pipe(d1, d2) = pipe;
+
+                    if d1.get_opposite() == direction || d2.get_opposite() == direction {
+                        Some(pipe)
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            })
+    }
+
+    fn find_start(&self) -> Coordinate {
+        self.0
+            .iter()
+            .enumerate()
+            .find_map(|(y, row)| {
+                row.iter()
+                    .position(|tile| tile == &Tile::Start)
+                    .map(|x| Coordinate(x as _, y as _))
+            })
+            .expect("Start tile should exist")
+    }
+}
 
 impl From<&str> for Maze {
     fn from(string: &str) -> Self {
@@ -16,27 +64,63 @@ impl From<&str> for Maze {
 
 impl std::fmt::Debug for Maze {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Display::fmt(
-            &self
-                .0
-                .iter()
-                .map(|row| row.iter().map(|tile| tile.to_string()).collect::<String>())
-                .collect::<Vec<_>>()
-                .join("\n"),
-            f,
-        )
+        let maze = &self
+            .0
+            .iter()
+            .map(|row| row.iter().map(|tile| tile.to_string()).collect::<String>())
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        Display::fmt(maze, f)
     }
 }
 
-enum Pipe {
-    Vertcial,
-    Horizontal,
-    NorthEast,
-    NorthWest,
-    SouthWest,
-    SouthEast,
+#[derive(Debug, PartialEq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
 }
 
+impl Direction {
+    fn get_delta(&self) -> Coordinate {
+        match self {
+            North => Coordinate(0, -1),
+            East => Coordinate(1, 0),
+            South => Coordinate(0, 1),
+            West => Coordinate(-1, 0),
+        }
+    }
+
+    fn get_opposite(&self) -> Self {
+        match self {
+            North => South,
+            East => West,
+            South => North,
+            West => East,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Pipe(Direction, Direction);
+
+impl Display for Pipe {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pipe(North, South) => write!(f, "║"),
+            Pipe(East, West) => write!(f, "═"),
+            Pipe(North, East) => write!(f, "╚"),
+            Pipe(North, West) => write!(f, "╝"),
+            Pipe(South, West) => write!(f, "╗"),
+            Pipe(South, East) => write!(f, "╔"),
+            Pipe(d1, d2) => panic!("Unexpected pipe '{:?}/{:?}' found", d1, d2),
+        }
+    }
+}
+
+#[derive(PartialEq)]
 enum Tile {
     Pipe(Pipe),
     Ground,
@@ -46,12 +130,12 @@ enum Tile {
 impl From<char> for Tile {
     fn from(c: char) -> Self {
         match c {
-            '|' => Tile::Pipe(Pipe::Vertcial),
-            '-' => Tile::Pipe(Pipe::Horizontal),
-            'L' => Tile::Pipe(Pipe::NorthEast),
-            'J' => Tile::Pipe(Pipe::NorthWest),
-            '7' => Tile::Pipe(Pipe::SouthWest),
-            'F' => Tile::Pipe(Pipe::SouthEast),
+            '|' => Tile::Pipe(Pipe(North, South)),
+            '-' => Tile::Pipe(Pipe(East, West)),
+            'L' => Tile::Pipe(Pipe(North, East)),
+            'J' => Tile::Pipe(Pipe(North, West)),
+            '7' => Tile::Pipe(Pipe(South, West)),
+            'F' => Tile::Pipe(Pipe(South, East)),
             '.' => Tile::Ground,
             'S' => Tile::Start,
             other => panic!("Unexpected character '{}' found", other),
@@ -62,29 +146,97 @@ impl From<char> for Tile {
 impl Display for Tile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Pipe(pipe) => match pipe {
-                Pipe::Vertcial => write!(f, "║"),
-                Pipe::Horizontal => write!(f, "═"),
-                Pipe::NorthEast => write!(f, "╚"),
-                Pipe::NorthWest => write!(f, "╝"),
-                Pipe::SouthWest => write!(f, "╗"),
-                Pipe::SouthEast => write!(f, "╔"),
-            },
+            Self::Pipe(pipe) => pipe.fmt(f),
             Self::Ground => write!(f, " "),
             Self::Start => write!(f, "S"),
         }
     }
 }
 
+// let _ = [North, East, South, West].iter().filter(|&direction| {
+//     let todo!();
+// });
+
 fn main() {
     let data = indoc! {"
-        .....
-        .F-7.
-        .|.|.
-        .L-J.
-        .....
+        ..F7.
+        .FJ|.
+        SJ.L7
+        |F--J
+        LJ...
     "};
 
     let maze: Maze = data.into();
-    println!("{:?}", maze);
+    let start = maze.find_start();
+    let next = maze.get_connected_pipe(&start, East);
+    println!("next = {:#?}", next);
+    println!("start = {:#?}", start);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Coordinate, Direction, Maze, Pipe};
+    use indoc::indoc;
+    use Direction::{East, North, South, West};
+
+    #[test]
+    fn identifies_continuing_pipe() {
+        let maze: Maze = indoc! {"
+            .....
+            .F-7.
+            .|.|.
+            .L-J.
+            .....
+        "}
+        .into();
+
+        let top_left = &Coordinate(1, 1);
+
+        assert_eq!(
+            maze.get_connected_pipe(top_left, East),
+            Some(&Pipe(East, West))
+        );
+        assert_eq!(
+            maze.get_connected_pipe(top_left, South),
+            Some(&Pipe(North, South))
+        );
+        assert!(maze.get_connected_pipe(top_left, West).is_none());
+        assert!(maze.get_connected_pipe(top_left, North).is_none());
+
+        let bottom_right = &Coordinate(3, 3);
+
+        assert_eq!(
+            maze.get_connected_pipe(bottom_right, West),
+            Some(&Pipe(East, West))
+        );
+        assert_eq!(
+            maze.get_connected_pipe(bottom_right, North),
+            Some(&Pipe(North, South))
+        );
+    }
+
+    #[test]
+    fn finds_start() {
+        let maze: Maze = indoc! {"
+            .....
+            .S-7.
+            .|.|.
+            .L-J.
+            .....
+        "}
+        .into();
+
+        assert_eq!(maze.find_start(), Coordinate(1, 1));
+
+        let maze: Maze = indoc! {"
+            .....
+            .F-7.
+            .|.|.
+            .LSJ.
+            .....
+        "}
+        .into();
+
+        assert_eq!(maze.find_start(), Coordinate(2, 3));
+    }
 }
