194f1f05bacb0b51de541c9fb3d58d87bb62a667
Melker Ulander <melker.ulander@pm.me>
2024-12-06 22:29:47 +0100

    2024 5.2: completed

--- 2024/src/bin/day5.rs
+++ 2024/src/bin/day5.rs
@@ -1,60 +1,136 @@
-type Page = u32;
+use std::{
+    cmp::{self},
+    fmt::Debug,
+    rc::Rc,
+};
 
+// Part 1
 pub fn sum_middle_page_numbers(input: &str) -> u32 {
     get_valid_page_lines(input)
-        .map(|line| {
-            let middle = line.len() / 2;
-            line[middle]
+        .map(|line| get_line_middle(&line).number)
+        .sum()
+}
+
+// Part 2
+pub fn sum_middle_of_sorted_page_numbers(input: &str) -> u32 {
+    get_invalid_page_lines(input)
+        .map(|mut line| {
+            sort(&mut line);
+            get_line_middle(&line).number
         })
         .sum()
 }
 
-pub fn get_valid_page_lines(input: &str) -> impl Iterator<Item = Vec<Page>> {
-    let (ordering_rules, valid_pages) = parse(input);
+#[derive(Clone)]
+struct Page {
+    number: u32,
+    rules: Rc<Vec<(u32, u32)>>,
+}
+
+impl PartialEq for Page {
+    fn eq(&self, other: &Self) -> bool {
+        self.number == other.number
+    }
+}
+
+impl Debug for Page {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Page")
+            .field("number", &self.number)
+            .finish()
+    }
+}
+
+impl PartialOrd for Page {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        let self_is_less = self
+            .rules
+            .iter()
+            .find(|(before, after)| *before == self.number && *after == other.number);
+
+        if self_is_less.is_some() {
+            return Some(cmp::Ordering::Less);
+        }
+
+        let other_is_less = self
+            .rules
+            .iter()
+            .find(|(before, after)| *before == other.number && *after == self.number);
+
+        if other_is_less.is_some() {
+            return Some(cmp::Ordering::Greater);
+        }
+
+        unreachable!("Every page should have an ordering tule");
+    }
+}
+
+fn get_line_middle<T: Clone>(line: &[T]) -> T {
+    let middle = line.len() / 2;
+    line[middle].clone()
+}
+
+fn sort(pages: &mut [Page]) {
+    pages.sort_by(|a, b| a.partial_cmp(b).expect("Input always be sortable"))
+}
+
+fn get_valid_page_lines(input: &str) -> impl Iterator<Item = Vec<Page>> {
+    let valid_pages = parse(input);
     valid_pages
         .into_iter()
-        .filter(move |pages_line| is_valid_page_line(pages_line, &ordering_rules))
+        .filter(|pages_line| is_valid_page_line(pages_line))
 }
 
-fn parse(input: &str) -> (Vec<(Page, Page)>, Vec<Vec<Page>>) {
-    let (ordering_rules, pages) = input.split_once("\n\n").expect("Input has correct format");
-    let ordering_rules: Vec<_> = ordering_rules
-        .lines()
-        .map(|rule| {
-            let (before, after) = rule.split_once("|").unwrap();
-            let before = before.parse().expect("Must be numeric");
-            let after = after.parse().expect("Must be numeric");
-            (before, after)
-        })
-        .collect();
+fn get_invalid_page_lines(input: &str) -> impl Iterator<Item = Vec<Page>> {
+    let valid_pages = parse(input);
+    valid_pages
+        .into_iter()
+        .filter(|pages_line| !is_valid_page_line(pages_line))
+}
 
+fn parse(input: &str) -> Vec<Vec<Page>> {
+    let (ordering_rules, pages) = input.split_once("\n\n").expect("Input has correct format");
+    let ordering_rules: Vec<_> = ordering_rules.lines().map(parse_rule).collect();
+
+    let ordering_rules = Rc::new(ordering_rules);
     let valid_page_lines = pages
         .lines()
-        .map(|line| {
-            line.split(",")
-                .map(|page| page.parse().expect("Must be numeric"))
-                .collect()
-        })
+        .map(|line| parse_pages_line(line, ordering_rules.clone()))
         .collect();
 
-    (ordering_rules, valid_page_lines)
+    valid_page_lines
 }
 
-fn is_valid_page_line(pages_line: &[Page], ordering_rules: &[(Page, Page)]) -> bool {
+fn parse_rule(rule: &str) -> (u32, u32) {
+    let (before, after) = rule.split_once("|").unwrap();
+    let before = before.parse().expect("Must be numeric");
+    let after = after.parse().expect("Must be numeric");
+
+    (before, after)
+}
+
+fn parse_pages_line(line: &str, ordering_rules: Rc<Vec<(u32, u32)>>) -> Vec<Page> {
+    line.split(",")
+        .map(|page| {
+            let number = page.parse().expect("Must be numeric");
+            let rules = Rc::clone(&ordering_rules);
+            Page { number, rules }
+        })
+        .collect()
+}
+
+fn is_valid_page_line(pages_line: &[Page]) -> bool {
     pages_line
         .iter()
         .enumerate()
-        .map(|(page_index, &page)| {
-            let mut relevant_page_rules = ordering_rules
-                .iter()
-                .filter(|(before, after)| *after == page);
+        .map(|(page_index, page)| {
+            let mut relevant_page_rules =
+                page.rules.iter().filter(|(_, after)| *after == page.number);
 
-            let is_valid = relevant_page_rules.all(|(before, after)| {
+            relevant_page_rules.all(|(before, _)| {
                 let mut trailing_pages = pages_line.iter().skip(page_index + 1);
-
-                !trailing_pages.any(|&page| page == *before)
-            });
-            is_valid
+                !trailing_pages.any(|page| page.number == *before)
+            })
         })
         .all(|valid_page| valid_page)
 }
@@ -63,6 +139,7 @@ fn main() {
     let data = include_str!("../../data/day5");
 
     println!("Part 1: {}", sum_middle_page_numbers(data));
+    println!("Part 2: {}", sum_middle_of_sorted_page_numbers(data));
 }
 
 #[cfg(test)]
@@ -104,11 +181,10 @@ mod tests {
 
     #[test]
     fn invalidates_invalid_line() {
-        let (ordering_rules, page_lines) = parse(INPUT);
+        let page_lines = parse(INPUT);
         let invalid_line = page_lines.into_iter().nth(3).unwrap();
-        dbg!(&invalid_line);
 
-        assert!(!is_valid_page_line(&invalid_line, &ordering_rules))
+        assert!(!is_valid_page_line(&invalid_line))
     }
 
     #[test]
@@ -119,12 +195,58 @@ mod tests {
             vec![75, 47, 61, 53, 29],
             vec![97, 61, 53, 29, 13],
             vec![75, 29, 13],
-        ];
+        ]
+        .map(|pages_line| {
+            pages_line
+                .into_iter()
+                .map(|number| {
+                    let rules = Rc::new(vec![]);
+                    Page { number, rules }
+                })
+                .collect::<Vec<_>>()
+        });
+
         assert_equal(expected, valid_pages);
     }
 
     #[test]
     fn sums_middle_page_numbers() {
         assert_eq!(143, sum_middle_page_numbers(INPUT));
+    }
+
+    #[test]
+    fn compares_pages() {
+        let rules = parse(INPUT);
+        let rules = &rules.first().unwrap().first().unwrap().rules;
+
+        for (before, after) in rules.iter() {
+            let page1 = Page {
+                number: *before,
+                rules: rules.clone(),
+            };
+            let page2 = Page {
+                number: *after,
+                rules: rules.clone(),
+            };
+
+            assert!(page1 < page2);
+        }
+    }
+
+    #[test]
+    fn sorts_pages() {
+        let rules = parse(INPUT);
+        let rules = &rules.first().unwrap().first().unwrap().rules;
+
+        let mut pages = [61, 13, 29].map(|number| Page {
+            number,
+            rules: rules.clone(),
+        });
+        pages.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    }
+
+    #[test]
+    fn sums_middle_of_sorted_page_numbers() {
+        assert_eq!(123, sum_middle_of_sorted_page_numbers(INPUT));
     }
 }
